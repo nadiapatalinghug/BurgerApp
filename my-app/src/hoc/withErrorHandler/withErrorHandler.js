61892f554869466f7ac16904f275163064b1a1bb
Nadia Patalinghug <npatalinghug@future-processing.com>
2021-02-23 17:54:45 +0800

    added componentWillUnmount axios interceptors

--- my-app/src/hoc/withErrorHandler/withErrorHandler.js
+++ my-app/src/hoc/withErrorHandler/withErrorHandler.js
@@ -19,7 +19,8 @@ const withErrorHandler = ( WrappedComponent, axios ) => {
         }
 
         componentWillUnmount() {
-            
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
         }
 
         errorConfirmedHandler = () => {
